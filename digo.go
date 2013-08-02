772b5f02e96844d39e08e4f98fce51b1eb37a00c
Fabrice Aneche <akh@nobugware.com>
2013-08-02 13:55:06 +0200

    working version for int and string

--- digo.go
+++ digo.go
@@ -18,6 +18,10 @@ var (
 	minMaxRegex *regexp.Regexp
 )
 
+func init() {
+	log.SetFlags(log.Lshortfile | log.Ltime)
+}
+
 func emailField(email string, fieldName string) error {
 	if emailRegex == nil {
 		emailRegex = regexp.MustCompile(EMAIL_REG)
@@ -28,48 +32,59 @@ func emailField(email string, fieldName string) error {
 	return nil
 }
 
-func minMax(field interface{}, fieldName string, min, max int) error {
-	v := reflect.ValueOf(field)
+func minMax(field interface{}, fieldName string, dstType string, min, max int) error {
 
-	//if v.Type().Name() != "string" ...
-	// this is useless pass until we are scanning the payload ourself
+	switch dstType {
+	case "int":
+		if int(field.(float64)) < min {
+			return errors.New("field: " + fieldName + " is too small")
+		}
+		if int(field.(float64)) > max {
+			return errors.New("field: " + fieldName + " is too big")
+		}
+	case "string":
+		if reflect.ValueOf(field).Len() < min {
+			return errors.New("field: " + fieldName + " is too short")
+		}
+		if reflect.ValueOf(field).Len() > max {
+			return errors.New("field: " + fieldName + " is too long")
+		}
 
-	if v.Len() < min {
-		return errors.New("field: " + fieldName + " is too short")
 	}
-	if v.Len() > max {
-		return errors.New("field: " + fieldName + " is too long")
-	}
+
 	return nil
 }
 
 func UnmarshalJSON(data []byte, dst interface{}) error {
-	err := json.Unmarshal(data, dst)
+	var in interface{}
+
+	err := json.Unmarshal(data, &in)
+
+	// map for in
+	im := in.(map[string]interface{})
 
 	if err != nil {
 		return err
 	}
 
+	// analyze the target struct
 	val := reflect.ValueOf(dst)
 	if val.Kind() != reflect.Ptr || val.Elem().Kind() != reflect.Struct {
 		return errors.New("digo: interface must be a pointer to struct")
 	}
 	val = val.Elem()
 
-	// TODO we should parse the dst interface first then ask for json ...
-
 	for i := 0; i < val.NumField(); i++ {
 		vf := val.Field(i)
 		tf := val.Type().Field(i)
 		tag := tf.Tag
 
-		log.Printf("Field Name: %s,\t Field Type: %s Field Value: %v,\t Tag Value: %s\n",
-			tf.Name,
-			tf.Type,
-			vf.Interface(),
-			tag.Get("digo"))
-
+		// no tag for us no decoding
+		if tag.Get("digo") == "" {
+			continue
+		}
 		stags := strings.Split(tag.Get("digo"), ",")
+
 		// nothing to validate
 		if len(stags) < 2 {
 			return errors.New("field: " + tf.Name + " is incorrect")
@@ -77,40 +92,94 @@ func UnmarshalJSON(data []byte, dst interface{}) error {
 		fieldName := strings.TrimSpace(stags[0])
 		fieldType := strings.TrimSpace(stags[1])
 
-		if strings.HasPrefix(fieldType, "emailfield") {
-			if err := emailField(vf.Interface().(string), fieldName); err != nil {
-				return err
+		// log.Printf("SRCfield value: %s, SRCfield type :%T\nDSTField Name: %s, DSTField Type: %s Tag Value: %s",
+		// 	im[fieldName],
+		// 	im[fieldName],
+		// 	tf.Name,
+		// 	tf.Type,
+		// 	tag.Get("digo"))
+
+		// we need to manually set the dst type at least for numbers (defaulting to float64)
+		var dstType string
+		// realvalue in the decoded json
+		rv, _ := im[fieldName]
+		if rv != nil {
+			if strings.HasPrefix(fieldType, "emailfield") {
+				dstType = "string"
+				// check the src type
+				if reflect.TypeOf(rv).String() != "string" {
+					return errors.New("field: " + fieldName + " is not a string")
+				}
+				// check the dst type
+				if tf.Type.String() != "string" {
+					return errors.New("struct: " + tf.Name + " is not a string")
+				}
+				if err := emailField(rv.(string), fieldName); err != nil {
+					return err
+				}
+
+			} else if strings.HasPrefix(fieldType, "stringfield") {
+				dstType = "string"
+				// check the src type
+				if reflect.TypeOf(rv).String() != "string" {
+					return errors.New("field: " + fieldName + " is not a string")
+				}
+				// check the dst type
+				if tf.Type.String() != "string" {
+					return errors.New("struct: " + tf.Name + " is not a string")
+				}
+			} else if strings.HasPrefix(fieldType, "intfield") {
+				dstType = "int"
+				// check the src type
+				// all numbers default to float 64
+				if reflect.TypeOf(rv).String() != "float64" {
+					return errors.New("field: " + fieldName + " is not an integer")
+				}
+				// check the dst type
+				if tf.Type.String() != "int" {
+					return errors.New("struct: " + tf.Name + " is not an integer")
+				}
+			} else {
+				return errors.New("field: " + fieldName + " invalid digo type")
 			}
-		} else if strings.HasPrefix(fieldType, "stringfield") {
-			//TODO: ensure field type
-		} else {
-			return errors.New("field: " + fieldName + " invalid digo type")
+
+			// set the target value
+			switch dstType {
+			case "string":
+				vf.SetString(rv.(string))
+			case "int":
+				vf.SetInt(int64(rv.(float64)))
+			}
 		}
 
-		if len(stags) < 3 {
-			continue
-		}
-		log.Println(stags)
-
+		// check the target value is a same type
 		for _, stag := range stags[2:] {
 			stag = strings.TrimSpace(stag)
+
+			// tag required
 			if strings.HasPrefix(stag, "required") {
-				log.Println("LLAAA")
-				if vf.Interface().(string) == "" {
+				if rv == nil {
 					return errors.New("field: " + fieldName + " is required")
+				}
+				// for each type apply revelant test
+				if tf.Type.String() == "string" {
+					if rv.(string) == "" {
+						return errors.New("field: " + fieldName + " is required")
+					}
 				}
 			} else if strings.HasPrefix(stag, "minmax") {
 				if minMaxRegex == nil {
 					minMaxRegex = regexp.MustCompile(MINMAX_REG)
 				}
 				res := minMaxRegex.FindStringSubmatch(stag)
-				log.Println(res)
 				if len(res) != 3 {
 					return errors.New("field: " + fieldName + " minmax invalid call")
 				}
+				// no need to read err, regexp already check that for us
 				min, _ := strconv.Atoi(res[1])
 				max, _ := strconv.Atoi(res[2])
-				if err = minMax(vf.Interface(), fieldName, min, max); err != nil {
+
+				if err = minMax(rv, fieldName, dstType, min, max); err != nil {
 					return err
 				}
 
@@ -118,7 +187,6 @@ func UnmarshalJSON(data []byte, dst interface{}) error {
 
 		}
 
-		log.Println(stags)
 	}
 
 	return nil
--- digo_test.go
+++ digo_test.go
@@ -5,21 +5,35 @@ import (
 )
 
 func Test_EmailField(t *testing.T) {
+	assertEqual := func(val interface{}, exp interface{}) {
+		if val != exp {
+			t.Errorf("Expected %v, got %v.", exp, val)
+		}
+	}
 	type User struct {
 		Email string `digo:"email,emailfield()"`
 	}
 
-	b := []byte(`{"Email":"toto"}`)
+	b := []byte(`{"email":"toto"}`)
 	var u User
 	err := UnmarshalJSON(b, &u)
-	if err.Error() != "field: email is not a valid email address" {
-		t.Error("email test failed")
-	}
-	b = []byte(`{"Email":"toto@gmail.com"}`)
+	assertEqual(err.Error(), "field: email is not a valid email address")
+	b = []byte(`{"email":"toto@gmail.com"}`)
 	if err = UnmarshalJSON(b, &u); err != nil {
 		t.Error("should be a valid email")
 	}
 
+	b = []byte(`{"email":2}`)
+	err = UnmarshalJSON(b, &u)
+	assertEqual(err.Error(), "field: email is not a string")
+
+	type BadUser struct {
+		Email int `digo:"email,emailfield()"`
+	}
+	var bu BadUser
+	b = []byte(`{"email":"akh@titi.fr"}`)
+	err = UnmarshalJSON(b, &bu)
+	assertEqual(err.Error(), "struct: Email is not a string")
 }
 
 func Test_NoDigoType(t *testing.T) {
@@ -36,21 +50,59 @@ func Test_NoDigoType(t *testing.T) {
 }
 
 func Test_Required(t *testing.T) {
+	assertEqual := func(val interface{}, exp interface{}) {
+		if val != exp {
+			t.Errorf("Expected %v, got %v.", exp, val)
+		}
+	}
 	type User struct {
-		Name string `digo:"name,stringfield(), required()"`
+		Name string `digo:"name,stringfield(),required()"`
 	}
 
 	b := []byte(`{"error":"toto"}`)
 	var u User
 	err := UnmarshalJSON(b, &u)
-	if err.Error() != "field: name is required" {
-		t.Error("required field not detected")
+
+	assertEqual(err.Error(), "field: name is required")
+	b = []byte(`{"name":""}`)
+	err = UnmarshalJSON(b, &u)
+
+	assertEqual(err.Error(), "field: name is required")
+}
+
+func Test_Intfield(t *testing.T) {
+	assertEqual := func(val interface{}, exp interface{}) {
+		if val != exp {
+			t.Errorf("Expected %v, got %v.", exp, val)
+		}
 	}
+	type UserInt struct {
+		Uid int `digo:"uid,intfield()"`
+	}
+
+	var ui UserInt
+	b := []byte(`{"uid":"toto"}`)
+	err := UnmarshalJSON(b, &ui)
+	assertEqual(err.Error(), "field: uid is not an integer")
+
+	type User struct {
+		Uid string `digo:"uid,intfield()"`
+	}
+	var u User
+	b = []byte(`{"uid":3}`)
+	err = UnmarshalJSON(b, &u)
+	assertEqual(err.Error(), "struct: Uid is not an integer")
 }
 
 func Test_MinMax(t *testing.T) {
+	assertEqual := func(val interface{}, exp interface{}) {
+		if val != exp {
+			t.Errorf("Expected %v, got %v.", exp, val)
+		}
+	}
+
 	type User struct {
-		Name string `digo:"name,stringfield(), minmax(2|5)"`
+		Name string `digo:"name,stringfield(),minmax(2|5)"`
 	}
 
 	b := []byte(`{"name":"toto"}`)
@@ -59,4 +111,18 @@ func Test_MinMax(t *testing.T) {
 	if err != nil {
 		t.Error("should not detect an error")
 	}
+
+	type UserInt struct {
+		Uid int `digo:"uid,intfield(),minmax(2|5)"`
+	}
+	var ui UserInt
+	b = []byte(`{"uid":4}`)
+	err = UnmarshalJSON(b, &ui)
+	if err != nil {
+		t.Error("should not detect an error")
+	}
+
+	b = []byte(`{"uid":7}`)
+	err = UnmarshalJSON(b, &ui)
+	assertEqual(err.Error(), "field: uid is too big")
 }
